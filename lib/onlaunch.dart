b1c454ad04c6033beb64d836731b70d50bcba490
Lukas Baronyai <lukas@cityriddler.com>
2023-07-03 12:11:33 +0200

    init

---
+++ lib/onlaunch.dart
@@ -0,0 +1,4 @@
+/// OnLaunch is a service allowing app developers to notify app users about updates, warnings and maintenance Our open-source framework provides an easy-to-integrate client to communicate with the backend and display the user interface.
+library onlaunch;
+
+export 'src/onlaunch.dart';
---
+++ test/src/onlaunch_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:onlaunch/onlaunch.dart';
+
+void main() {
+  group('Onlaunch', () {
+    test('can be instantiated', () {
+      expect(Onlaunch(), isNotNull);
+    });
+  });
+}
